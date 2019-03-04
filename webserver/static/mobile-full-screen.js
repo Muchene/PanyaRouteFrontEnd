13882322a0999d5116a3467d390e5cc7199a9acb
David Muchene <david.muchene@gmail.com>
2019-03-04 10:32:43 -0500

    map channel

--- webserver/static/mobile-full-screen.js
+++ webserver/static/mobile-full-screen.js
@@ -4,7 +4,61 @@ import View from 'ol/View.js';
 import TileLayer from 'ol/layer/Tile.js';
 import BingMaps from 'ol/source/BingMaps.js';
 
-MapChannel.postMessage("TRYING TO PASS THIS MESSAGE");
+
+
+var isFlutter = false;
+if (typeof MapChannel != "undefined") {
+    isFlutter = true;
+    MapChannel.postMessage("TRYING TO PASS THIS MESSAGE");
+}else {
+    console.log("Viewing page from a non-flutter browser")
+}
+
+
+var utils = {
+    getNearest: function(coord){
+      var coord4326 = utils.to4326(coord);    
+      return new Promise(function(resolve, reject) {
+        //make sure the coord is on street
+        fetch(url_osrm_nearest + coord4326.join()).then(function(response) { 
+          // Convert to JSON
+          return response.json();
+        }).then(function(json) {
+          if (json.code === 'Ok') resolve(json.waypoints[0].location);
+          else reject();
+        });
+      });
+    },
+    createFeature: function(coord) {
+      var feature = new ol.Feature({
+        type: 'place',
+        geometry: new ol.geom.Point(ol.proj.fromLonLat(coord))
+      });
+      feature.setStyle(styles.icon);
+      vectorSource.addFeature(feature);
+    },
+    createRoute: function(polyline) {
+      // route is ol.geom.LineString
+      var route = new ol.format.Polyline({
+        factor: 1e5
+      }).readGeometry(polyline, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: 'EPSG:3857'
+      });
+      var feature = new ol.Feature({
+        type: 'route',
+        geometry: route
+      });
+      feature.setStyle(styles.route);
+      vectorSource.addFeature(feature);
+    },
+    to4326: function(coord) {
+      return ol.proj.transform([
+        parseFloat(coord[0]), parseFloat(coord[1])
+      ], 'EPSG:3857', 'EPSG:4326');
+    }
+  };
+
 
 const view = new View({
   center: [0, 0],
